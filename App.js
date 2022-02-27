99fd560ce30a5c9cf91774169defb11b1f81b253
creatorckim <calvinkim4@yahoo.com>
2022-02-27 02:03:17 -0500

    Created exercise list screen

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './components/HomeScreen';
+import ExListScreen from './components/ExListScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName='Routine'>
+        <Stack.Screen name='Routine' component={HomeScreen} options={({navigation}) => ({ headerStyle: {backgroundColor: '#fff'},  headerRight: () => ( <TouchableOpacity style={styles.exListBtn} onPress={() => {navigation.navigate('Exercise List')}}></TouchableOpacity>
+        )})}/>
+        <Stack.Screen name='Exercise List' component={ExListScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  exListBtn: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#08964F',
+  }
 });
---
+++ components/ExListScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+
+const db = openDatabase('exercise-db');
+
+function ExListScreen() {
+    return (
+        <View>
+            <ScrollView>
+
+            </ScrollView>
+            <TouchableOpacity><Text>ADD</Text></TouchableOpacity>
+        </View>
+    );
+}
+
+export default ExListScreen;
